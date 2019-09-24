c4aedca541e1f3f1dfe74f8c20d6114770228fae
Alberto <garre.alberto@gmail.com>
2019-09-24 17:34:00 +0200

    Corrected a bug in the simulations
    I was using sigma^2 instad of sigma

--- stat_functions.R
+++ stat_functions.R
@@ -4,7 +4,7 @@ simulate_ph <- function(n, mu, s, sim_dim1 = 2000, sim_dim2 = 5000) {
     tibble(t_val = rt(sim_dim1, n-1),
            mu_sim = t_val*s/sqrt(n) + mu,
            # mu_sim = rnorm(sim_dim1, mu, s/sqrt(n)),
-           s_sim = (n-1)*s^2/rchisq(sim_dim1, n-1)) %>%
+           s_sim = sqrt((n-1)*s^2/rchisq(sim_dim1, n-1))) %>%
         # ggplot() +
         #     geom_density(aes(s_sim))
         mutate(sim = 1:nrow(.)) %>%
